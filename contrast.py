4cc94d21cbf4349a115f0a7388044e1bb62f2464
seacrab808 <seacrab808@naver.com>
2024-09-06 11:20:52 +0900

    Feat: contrast, normalize, 명도 조절 등

---
+++ contrast.py
@@ -0,0 +1,26 @@
+# cv2.normalize
+
+import cv2, sys
+import numpy as np
+
+src = cv2.imread('data2/Hawkes.jpg', cv2.IMREAD_GRAYSCALE)
+
+if src is None:
+    sys.exit("이미지 로드 실패")
+    
+# src 이미지에서 최소값과 최대값을 확인
+#pixMin, pixMax, _, _ = cv2.minMaxLoc(src)
+# print(pixMin, pixMax)
+
+# 이미지를 정규화 0 ~ 255
+dst = cv2.normalize(src, None, 0, 255, cv2.NORM_MINMAX)
+pixMin, pixMax, _, _ = cv2.minMaxLoc(dst)
+print(pixMin, pixMax)
+# 데이터 결과를 파일로 저장
+cv2.imwrite('data2/Hawkes_norm.jpg', dst)
+
+# dst 이미지가 더 선명해짐
+cv2.imshow('img', src)
+cv2.imshow('dst', dst)
+cv2.waitKey()
+cv2.destroyAllWindows()
---
+++ exAddweighted.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+
+src1 = cv2.imread('data2/airplane.bmp')
+src2 = cv2.imread('data2/field.bmp')
+
+# alpha값이 높을 수록 진해짐
+alpha = 0.7
+beta = 1 - alpha
+
+dst1 = cv2.addWeighted(src1, alpha=alpha, src2=src2, beta=beta, gamma=0)
+    
+cv2.imshow('img1', src1)
+cv2.imshow('img2', src2)
+cv2.imshow('dst1', dst1)
+cv2.waitKey()
+cv2.destroyAllWindows()
